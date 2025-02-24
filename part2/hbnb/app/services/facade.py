41b17a31fd516fb66c79ade5493e50fd22596975
vidad <vidad31@gmail.com>
2025-02-24 17:45:43 +0100

    Implement user creation and retrieval methods in HBnBFacade

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -1,18 +1,24 @@
 from app.persistence.repository import InMemoryRepository
+from app.models.user import User
+
 
 class HBnBFacade:
     def __init__(self):
+        """ Initializes a new instance of HBnBFacade.
+        Creates an in-memory repository for users.
+        """
         self.user_repo = InMemoryRepository()
-        self.place_repo = InMemoryRepository()
-        self.review_repo = InMemoryRepository()
-        self.amenity_repo = InMemoryRepository()
 
-    # Placeholder method for creating a user
     def create_user(self, user_data):
-        # Logic will be implemented in later tasks
-        pass
+        """ Create a new user and add it to the repository."""
+        user = User(**user_data)
+        self.user_repo.add(user)
+        return user
 
-    # Placeholder method for fetching a place by ID
-    def get_place(self, place_id):
-        # Logic will be implemented in later tasks
-        pass
+    def get_user(self, user_id):
+        """ Retrieves a user by their ID."""
+        return self.user_repo.get(user_id)
+
+    def get_user_by_email(self, email):
+        """ Retrieves a user by their email."""
+        return self.user_repo.get_by_attribute("email", email)
