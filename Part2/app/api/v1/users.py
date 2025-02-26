746bf4f60886a0e6bcfd1c168a3c49c266179a05
vidad <vidad31@gmail.com>
2025-02-26 18:50:11 +0100

    Refactor user response formatting and add email uniqueness check during user update

--- Part2/app/api/v1/users.py
+++ Part2/app/api/v1/users.py
@@ -26,7 +26,12 @@ class UserList(Resource):
             return {'error': 'Email already registered'}, 400
         """Create a new user"""
         new_user = facade.create_user(user_data)
-        return {'id': new_user.id, 'first_name': new_user.first_name, 'last_name': new_user.last_name, 'email': new_user.email}, 201
+        return {
+                'id': new_user.id,
+                'first_name': new_user.first_name,
+                'last_name': new_user.last_name,
+                'email': new_user.email
+                }, 201
     
 @api.route('/<user_id>')
 class UserResource(Resource):
@@ -37,7 +42,12 @@ class UserResource(Resource):
         user = facade.get_user(user_id)
         if not user:
             return {'error': 'User not found'}, 404
-        return {'id': user.id, 'first_name': user.first_name, 'last_name': user.last_name, 'email': user.email}, 200
+        return {
+                'id': user.id, 
+                'first_name': user.first_name,
+                'last_name': user.last_name,
+                'email': user.email
+                }, 200
     
     @api.expect(user_model, validate=True)
     @api.response(200, 'User sucessfully updated')
@@ -50,7 +60,14 @@ class UserResource(Resource):
         user = facade.get_user(user_id)
         if not user:
             return {'error': 'User not found'}, 404
+
+        # Check if the email is already registered by another user
+        existing_user = facade.get_user_by_email(user_data['email'])
+        if existing_user and existing_user.id != user.id:
+            return {'error': 'Email already registered by another user'}, 400
         
         updated_user = facade.update_user(user_id, user_data)
-        return {'id': updated_user.id, 'first_name': updated_user.first_name, 'last_name': updated_user.last_name, 'email': updated_user.email}, 200
-    
+        return {'id': updated_user.id,
+                'first_name': updated_user.first_name,
+                'last_name': updated_user.last_name,
+                'email': updated_user.email}, 200
