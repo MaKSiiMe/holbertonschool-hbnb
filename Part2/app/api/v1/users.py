82d032ab105732a2ce51a8673b38e9f126d2ddd8
vidad <vidad31@gmail.com>
2025-02-26 10:50:24 +0100

    Add user update functionality and improve user creation response

--- Part2/app/api/v1/users.py
+++ Part2/app/api/v1/users.py
@@ -24,7 +24,7 @@ class UserList(Resource):
         existing_user = facade.get_user_by_email(user_data['email'])
         if existing_user:
             return {'error': 'Email already registered'}, 400
-
+        """Create a new user"""
         new_user = facade.create_user(user_data)
         return {'id': new_user.id, 'first_name': new_user.first_name, 'last_name': new_user.last_name, 'email': new_user.email}, 201
     
@@ -38,4 +38,19 @@ class UserResource(Resource):
         if not user:
             return {'error': 'User not found'}, 404
         return {'id': user.id, 'first_name': user.first_name, 'last_name': user.last_name, 'email': user.email}, 200
+    
+    @api.expect(user_model, validate=True)
+    @api.response(200, 'User sucessfully updated')
+    @api.response(404, 'User not found')
+    def put(self, user_id):
+        """Update user details by ID"""
+        user_data = api.payload
+        
+        """Check if the user exists"""
+        user = facade.get_user(user_id)
+        if not user:
+            return {'error': 'User not found'}, 404
+        
+        updated_user = facade.update_user(user_id, user_data)
+        return {'id': updated_user.id, 'first_name': updated_user.first_name, 'last_name': updated_user.last_name, 'email': updated_user.email}, 200
     
