0dd712e2d84958ea41ad1cd5d004ec07de733818
ludiane-tr <ludiane.trouillefou@gmail.com>
2025-02-27 14:03:10 +0100

    Add tests for Place creation API endpoint

---
+++ test/New-Place.py
@@ -0,0 +1,11 @@
+POST /api/v1/places/
+Content-Type: application/json
+
+{
+  "title": "Cozy Apartment",
+  "description": "A nice place to stay",
+  "price": 100.0,
+  "latitude": 37.7749,
+  "longitude": -122.4194,
+  "owner_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+}
