0346b6cbf26635412c9619f4ff82be001d0f2972
vidad <vidad31@gmail.com>
2025-02-27 16:05:26 +0100

    Add Places namespace to HBnB API

--- Part2/app/api/__init__.py
+++ Part2/app/api/__init__.py
@@ -7,4 +7,9 @@ api = Api(blueprint, title='HBnB API', version='1.0', description='API for HBnB'
 
 # Register namespaces
 from .v1.places import api as places_ns
-api.add_namespace(places_ns, path='/api/v1/places')  # Maps /api/v1/places endpoints
+api.add_namespace(places_ns, path='/api/v1/places') 
+
+from .v1.users import api as users_ns
+api.add_namespace(users_ns, path='/api/v1/users')
+
+ # Maps /api/v1/places endpoints
