ea0ca3b5b127d5b7b63474aa8df7735ec114231e
vidad <vidad31@gmail.com>
2025-02-28 18:44:48 +0100

    Add namespaces for amenities and reviews APIs in the main API initialization

--- Part2/app/api/__init__.py
+++ Part2/app/api/__init__.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+"""from flask import Blueprint
 from flask_restx import Api
 
 # Initialize blueprint and API
@@ -12,4 +12,9 @@ api.add_namespace(places_ns, path='/api/v1/places')
 from .v1.users import api as users_ns
 api.add_namespace(users_ns, path='/api/v1/users')
 
- # Maps /api/v1/places endpoints
+from .v1.amenities import api as amenities_ns
+api.add_namespace(amenities_ns, path='/api/v1/amenities')
+
+from .v1.reviews import api as reviews_ns
+api.add_namespace(reviews_ns, path='/api/v1/reviews')
+"""
