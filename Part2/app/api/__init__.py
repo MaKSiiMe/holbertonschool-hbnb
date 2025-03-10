102b6134ce6698eb900c082c0a0797f9df8a5b43
vidad <vidad31@gmail.com>
2025-03-10 10:00:07 +0100

    removing this code due to duplicates

--- Part2/app/api/__init__.py
+++ Part2/app/api/__init__.py
@@ -7,7 +7,7 @@ api = Api(blueprint, title='HBnB API', version='1.0', description='API for HBnB'
 
 # Register namespaces
 from .v1.places import api as places_ns
-api.add_namespace(places_ns, path='/api/v1/places') 
+api.add_namespace(places_ns, path='/places') 
 
 from .v1.users import api as users_ns
 api.add_namespace(users_ns, path='/api/v1/users')
@@ -16,5 +16,4 @@ from .v1.amenities import api as amenities_ns
 api.add_namespace(amenities_ns, path='/api/v1/amenities')
 
 from .v1.reviews import api as reviews_ns
-api.add_namespace(reviews_ns, path='/api/v1/reviews')
-"""
+api.add_namespace(reviews_ns, path='/api/v1/reviews')"""
