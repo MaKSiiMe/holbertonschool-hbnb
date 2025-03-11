5b1ab64fc93b1bec18e185758d1e5f2ba4c32e8c
MaKSiiMe <maxime.truel@gmail.com>
2025-03-11 16:56:36 +0100

    change init file

--- Part2/app/__init__.py
+++ Part2/app/__init__.py
@@ -6,6 +6,7 @@ from app.api.v1.places import api as places_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.reviews import api as reviews_ns
 
+
 def create_app():
     app = Flask(__name__)
 
