555be94e4a39f0dc0f2567798a64fc0906d7bac8
vidad <vidad31@gmail.com>
2025-03-13 11:56:01 +0100

    modif

--- Part3/config.py
+++ Part3/config.py
@@ -3,11 +3,24 @@ import os
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///development.db')
+    #SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///development.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False # Disable modification tracking for performance
+
+class TestingConfig(Config):
+    Testing = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'  # Use an in-memory database for testing
+
+class ProductionConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')  # Must be set via environment variable
+
 class DevelopmentConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///app.db')  # Default to SQLite if not set
 
 config = {
     'development': DevelopmentConfig,
-    'default': DevelopmentConfig
+    'default': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig
 }
