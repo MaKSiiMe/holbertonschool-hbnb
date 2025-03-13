2e455ee6f8d005ce28f87e7c9b17773327b42193
vidad <vidad31@gmail.com>
2025-03-13 11:56:43 +0100

    add Migrate and config from

--- Part3/app/__init__.py
+++ Part3/app/__init__.py
@@ -1,23 +1,32 @@
 from flask import Flask, Blueprint, redirect
 from flask_restx import Api
-from app.extensions import db, bcrypt, jwt
+from app.extensions import db, bcrypt, jwt, JWTManager
+from flask_migrate import Migrate
+from config import config
+
+#API Namespaces
 from app.api.v1.users import api as users_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.reviews import api as reviews_ns
+from app.api.v1.auth import api as auth_ns
 
-def create_app(config_class="config.DevelopmentConfig"):
+jwt = JWTManager()
+migrate = Migrate()
+
+def create_app(config_class="development"):
     app = Flask(__name__)
-    app.config.from_object(config_class)
+    app.config.from_object(config[config_class])
 
     # Initialize centralized extensions
     db.init_app(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
+    migrate.init_app(app, db)
 
     # Creates the database tables
-    with app.app_context():
-        db.create_all()
+    #with app.app_context():
+        #db.create_all()
 
     @app.route('/')
     def root():
@@ -28,6 +37,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     api = Api(api_blueprint, title='HBnb API', version='1.0', description='HBnB Application API', doc='/')
 
     # Register namespaces with the API
+    api.add_namespace(auth_ns, path='/auth')
     api.add_namespace(users_ns, path='/users')
     api.add_namespace(places_ns, path='/places')
     api.add_namespace(amenities_ns, path='/amenities')
