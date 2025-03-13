de5b60903ca865ffad826a4b360b5d1343dac828
vidad <vidad31@gmail.com>
2025-03-13 09:44:37 +0100

    modif fichier

--- Part3/config.py
+++ Part3/config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
     DEBUG = False
-
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///development.db')
 class DevelopmentConfig(Config):
     DEBUG = True
 
