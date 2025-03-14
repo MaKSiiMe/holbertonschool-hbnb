d06db45d5485198d0bf320d4debf799bb396f92a
vidad <vidad31@gmail.com>
2025-03-14 14:13:32 +0100

    delete def init

--- Part3/app/models/BaseModel.py
+++ Part3/app/models/BaseModel.py
@@ -9,11 +9,6 @@ class BaseModel(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
     def save(self):
         """Persist the object to the database."""
         db.session.add(self)
